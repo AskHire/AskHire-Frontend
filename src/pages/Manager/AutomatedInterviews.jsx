51c9c21dad828f43560804380a8a9e1ee11b2510
Dimashi2002 <dimashiwickramage2002@gmail.com>
2025-07-10 13:15:33 +0530

    Updated View Long list

--- src/pages/Manager/AutomatedInterviews.jsx
+++ src/pages/Manager/AutomatedInterviews.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useNavigate, useLocation } from "react-router-dom";
 
 const AutomatedInterviews = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const vacancy = params.get('vacancy');
+
   return (
     <div>AutomatedInterviews</div>
   )
