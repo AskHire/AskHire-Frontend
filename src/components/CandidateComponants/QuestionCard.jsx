f73b378b98ab8f23c86873f3ef3f339a823348af
JanithAberathna <dilshanjanith68@gmail.com>
2025-07-10 10:24:04 +0530

    updated TextAssessment Components

---
+++ src/components/CandidateComponants/QuestionCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FiCheckCircle } from 'react-icons/fi';
+
+const QuestionCard = ({ question, onSelectAnswer }) => {
+  return (
+    <div>
+      <p className="text-lg mb-4">{question.question}</p>
+      <div className="space-y-3">
+        {question.options.map((option) => (
+          <button
+            key={option.id}
+            onClick={() => onSelectAnswer(option.id)}
+            className={`w-full text-left p-4 rounded-lg border transition
+              ${question.selectedAnswer === option.id
+                ? 'bg-green-100 border-green-600 border-2 text-black'
+                : 'bg-white border-gray-300 hover:border-blue-400'}`}
+          >
+            <div className="flex justify-between items-center">
+              <span>{option.text}</span>
+              {question.selectedAnswer === option.id && (
+                <FiCheckCircle className="text-green-600" />
+              )}
+            </div>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
